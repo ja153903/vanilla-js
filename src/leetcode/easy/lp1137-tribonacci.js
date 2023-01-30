2d09372b9b3f4256c6fc096296b50f36a3107796
ja153903 <jaimeabbariao@gmail.com>
2023-01-30 16:47:05 -0500

    feat: updating tribonacci

---
+++ src/leetcode/easy/lp1137-tribonacci.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const tribonacci = function (n) {
+  let a = 0
+  let b = 1
+  let c = 1
+
+  if (n === 0) {
+    return a
+  }
+
+  if (n <= 2) {
+    return b
+  }
+
+  for (let i = 3; i <= n; i++) {
+    ;[a, b, c] = [b, c, a + b + c]
+  }
+
+  return c
+}
+
+module.exports = { tribonacci }
