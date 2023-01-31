eafe599ab183f39505d2ddc0906f1a5d07cda79b
ja153903 <jaimeabbariao@gmail.com>
2023-01-30 22:21:03 -0500

    feat: updating with daily

---
+++ src/leetcode/medium/lp1626-team-no-conflict.js
@@ -0,0 +1,34 @@
+/**
+ * This problem uses a variant of LIS
+ *
+ * @param {number[]} scores
+ * @param {number[]} ages
+ * @return {number}
+ */
+const bestTeamScore = function (scores, ages) {
+  const data = scores.map((score, index) => ({ score, age: ages[index] }))
+
+  data.sort((a, b) => {
+    if (a.age === b.age) {
+      return a.score - b.score
+    }
+    return a.age - b.age
+  })
+
+  const dp = new Array(data.length)
+    .fill(0)
+    .map((_value, index) => data[index].score)
+
+  for (let i = 1; i < data.length; i++) {
+    for (let j = 0; j < i; j++) {
+      // the value should only be added to the dp array
+      if (data[i].age >= data[j].age && data[i].score >= data[j].score) {
+        dp[i] = Math.max(dp[i], dp[j] + data[i].score)
+      }
+    }
+  }
+
+  return Math.max(...dp)
+}
+
+module.exports = { bestTeamScore }
