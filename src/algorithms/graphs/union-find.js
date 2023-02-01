d0f498a6f5c7aaf811dcaac1c784627a79ed8a14
ja153903 <jaimeabbariao@gmail.com>
2023-02-01 01:24:18 -0500

    feat: updating with problems

---
+++ src/algorithms/graphs/union-find.js
@@ -0,0 +1,73 @@
+/**
+ * @fileoverview Union-Find data structure
+ */
+class UnionFind {
+  /**
+   * @param {number} nodes
+   */
+  constructor(nodes) {
+    /** @type {number} */
+    this.nodes = nodes
+    /** @type {number[]} */
+    this.ranks = new Array(nodes).fill(0)
+    /** @type {number[]} */
+    this.parents = new Array(nodes).fill(0).map((_value, index) => index)
+  }
+
+  /**
+   * This function searches for the node's parent
+   * with path compression.
+   *
+   * @param {number} node
+   * @returns {number}
+   */
+  find(node) {
+    if (this.parents[node] !== node) {
+      this.parents[node] = this.find(this.parents[node])
+    }
+
+    return this.parents[node]
+  }
+
+  /**
+   * This function connects two nodes into the same
+   * connected component
+   *
+   * @param {number} u
+   * @param {number} v
+   */
+  union(u, v) {
+    const uParent = this.find(u)
+    const vParent = this.find(v)
+
+    if (uParent === vParent) {
+      return
+    }
+
+    if (this.ranks[uParent] < this.ranks[vParent]) {
+      this.parents[uParent] = vParent
+      this.ranks[vParent]++
+    } else {
+      this.parents[vParent] = uParent
+      this.ranks[uParent]++
+    }
+  }
+
+  /**
+   * This static methods gets the number of connected components
+   * in a Union-Find data structure
+   *
+   * @returns {number}
+   */
+  get numberOfConnectedComponents() {
+    const set = new Set()
+
+    for (let i = 0; i < this.nodes; i++) {
+      set.add(this.find(i))
+    }
+
+    return set.size
+  }
+}
+
+module.exports = { UnionFind }
---
+++ src/leetcode/easy/lp1071-string-divisor.js
@@ -0,0 +1,17 @@
+const { gcd } = require('@/algorithms/math')
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @returns {string}
+ */
+const gcdOfStrings = function (str1, str2) {
+  // if we can concatenate the strings in reverse order
+  // and get the same string, then we know that there is a gcd
+  // otherwise we return an empty string
+  return str1 + str2 === str2 + str1
+    ? str1.slice(0, gcd(str1.length, str2.length))
+    : ''
+}
+
+module.exports = { gcdOfStrings }
