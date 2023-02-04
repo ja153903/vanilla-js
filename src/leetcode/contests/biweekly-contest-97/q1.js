7a2258f249f2d23eeac48a72a91955919bd07b01
ja153903 <jaimeabbariao@gmail.com>
2023-02-04 17:50:19 -0500

    fix: reverting solution

--- src/leetcode/contests/biweekly-contest-97/q1.js
+++ src/leetcode/contests/biweekly-contest-97/q1.js
@@ -6,10 +6,14 @@ const separateDigits = function (nums) {
   let result = []
 
   for (let num of nums) {
+    const subresult = []
+
     while (num > 0) {
-      result.unshift(num % 10)
+      subresult.unshift(num % 10)
       num = Math.floor(num / 10)
     }
+
+    result = [...result, ...subresult]
   }
 
   return result
