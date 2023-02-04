070d5518947c860e344d83e9ebfaa82066a53be6
ja153903 <jaimeabbariao@gmail.com>
2023-02-04 17:49:54 -0500

    feat: updating logic

--- src/leetcode/contests/biweekly-contest-97/q1.js
+++ src/leetcode/contests/biweekly-contest-97/q1.js
@@ -6,14 +6,10 @@ const separateDigits = function (nums) {
   let result = []
 
   for (let num of nums) {
-    const subresult = []
-
     while (num > 0) {
-      subresult.unshift(num % 10)
+      result.unshift(num % 10)
       num = Math.floor(num / 10)
     }
-
-    result = [...result, ...subresult]
   }
 
   return result
