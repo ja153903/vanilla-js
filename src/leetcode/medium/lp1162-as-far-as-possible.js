99bd5bbbf3f66f15ff61d5e325681820b65b38d1
ja153903 <jaimeabbariao@gmail.com>
2023-02-10 16:29:16 -0500

    feat: updating with more problems

---
+++ src/leetcode/medium/lp1162-as-far-as-possible.js
@@ -0,0 +1,66 @@
+const DIRECTIONS = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+]
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const maxDistance = function (grid) {
+  // We want to create a queue of all the land cells and the current number
+  // of edges it has taken so far.
+  // we also want to memoize part of the grid
+
+  /** @type {{x: number, y: number}[]} */
+  const queue = []
+
+  /** @type {Set<string>} */
+  const visited = new Set()
+
+  const rows = grid.length
+  const cols = grid[0].length
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (grid[row][col] === 1) {
+        queue.push({ x: row, y: col })
+        visited.add(`${row},${col}`)
+      }
+    }
+  }
+
+  if (queue.length === 0 || queue.length === rows * cols) {
+    return -1
+  }
+
+  let max = -1
+
+  while (queue.length > 0) {
+    const size = queue.length
+    for (let i = 0; i < size; i++) {
+      const { x, y } = queue.shift()
+
+      for (const [dx, dy] of DIRECTIONS) {
+        const [nx, ny] = [x + dx, y + dy]
+        if (
+          nx >= 0 &&
+          nx < rows &&
+          ny >= 0 &&
+          ny < cols &&
+          !visited.has(`${nx},${ny}`)
+        ) {
+          visited.add(`${nx},${ny}`)
+          queue.push({ x: nx, y: ny })
+        }
+      }
+    }
+    max++
+  }
+
+  return max
+}
+
+export { maxDistance }
---
+++ src/leetcode/medium/lp904-fruits-into-baskets.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+const totalFruit = function (fruits) {
+  // keep track of how many fruits are currently in our window
+  let fruitsInWindow = 0
+
+  /** @type {Map<number, number>} */
+  const mp = new Map()
+
+  let start = 0
+
+  let result = 0
+
+  for (let end = 0; end < fruits.length; end++) {
+    const fruit = fruits[end]
+    if (!mp.has(fruit) || mp.get(fruit) === 0) {
+      fruitsInWindow++
+    }
+
+    mp.set(fruit, (mp.get(fruit) || 0) + 1)
+
+    if (fruitsInWindow <= 2) {
+      result = Math.max(result, end - start + 1)
+    }
+
+    while (fruitsInWindow > 2) {
+      // update the start of the window
+      const startFruit = fruits[start]
+      mp.set(startFruit, mp.get(startFruit) - 1)
+      if (mp.get(startFruit) === 0) {
+        fruitsInWindow--
+      }
+      start++
+    }
+  }
+
+  return result
+}
+
+export { totalFruit }
