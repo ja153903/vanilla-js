1305e7a17af0b62e0d7306ae445f0f94f72fda37
ja153903 <jaimeabbariao@gmail.com>
2023-03-12 12:36:37 -0400

    feat: updating daily temp problem

--- src/neetcode/stack/lp739-daily-temperatures.js
+++ src/neetcode/stack/lp739-daily-temperatures.js
@@ -5,6 +5,20 @@
 const dailyTemperatures = function (temperatures) {
   // this is a monotonic stack problem
   // because we're looking for the next greatest element
+  const n = temperatures.length
+  const result = new Array(n).fill(0)
+  const stack = []
+
+  for (let i = 0; i < n; i++) {
+    while (stack.length && temperatures[i] > temperatures[stack.at(-1)]) {
+      const item = stack.pop()
+      result[item] = i - item
+    }
+
+    stack.push(i)
+  }
+
+  return result
 }
 
 export { dailyTemperatures }
