edd5c404313075a74742071ed2f4d7770a7ca133
ja153903 <jaimeabbariao@gmail.com>
2023-03-12 23:57:21 -0400

    feat: solving koko problem

--- src/neetcode/binary-search/lp875-koko-eating-bananas.js
+++ src/neetcode/binary-search/lp875-koko-eating-bananas.js
@@ -3,6 +3,43 @@
  * @param {number} h
  * @return {number}
  */
-const minEatingSpeed = function (piles, h) {}
+const minEatingSpeed = function (piles, h) {
+  // k is the number of bananas koko eats from a pile
+  // the minimum k is 1 and the maximum k is the max pile size
+  let left = 1
+  let right = Math.max(...piles)
+
+  while (left < right) {
+    const mid = left + Math.floor((right - left) / 2)
+
+    // figure out if we can finish it
+    if (canFinish(piles, mid, h)) {
+      right = mid
+    } else {
+      left = mid + 1
+    }
+  }
+
+  return left
+}
+
+/**
+ * @param {number[]} piles
+ * @param {number} k
+ * @param {number} h
+ * @returns {boolean}
+ */
+function canFinish(piles, k, h) {
+  let hours = 0
+
+  for (const pile of piles) {
+    hours += Math.floor(pile / k)
+    if (pile % k) {
+      hours++
+    }
+  }
+
+  return hours <= h
+}
 
 export { minEatingSpeed }
